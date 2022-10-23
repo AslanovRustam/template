05bc42bf966cea2f65ff72b1d9a67fa19c89026d
Aslanov Rustam <aslanovrg@gmail.com>
2022-10-23 15:05:05 +0300

    trying to fix some sheet

--- src/components/ForbesListItem/ForbesListItem.jsx
+++ src/components/ForbesListItem/ForbesListItem.jsx
@@ -1,8 +1,8 @@
-import { BiDollarCircle } from 'react-icons/bi';
-import { FcBullish, FcBearish } from 'react-icons/fc';
-import { ForbesItem, Avatar, Name, Capital } from './ForbesListItem.styled';
+// import { BiDollarCircle } from 'react-icons/bi';
+// import { FcBullish, FcBearish } from 'react-icons/fc';
+// import { ForbesItem, Avatar, Name, Capital } from './ForbesListItem.styled';
 
-import { theme } from 'styles/theme';
+// import { theme } from 'styles/theme';
 
 export const ForbesListItem = () => {
   return <div>ForbesItem</div>;
